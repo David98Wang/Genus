aff98bcb272345d8ee2571e62e04bb1cdcaca792
David Wang <whcdavidca@gmail.com>
2017-06-28 00:08:10 -0400

    Added database connection. Initialized "Person" object

--- app/src/main/java/uwaterloo/ece/genus/MainActivity.java
+++ app/src/main/java/uwaterloo/ece/genus/MainActivity.java
@@ -3,11 +3,27 @@ package uwaterloo.ece.genus;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
+public class MainActivity extends AppCompatActivity {
+    FirebaseApp firebaseApp;
+    private DatabaseReference mDatabase;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initializeApp();
+    }
+
+    private void initializeApp() {
+        initializeDatabaseConnection();
+    }
+
+    private void initializeDatabaseConnection() {
+        mDatabase = FirebaseDatabase.getInstance().getReference();
+
     }
 }
